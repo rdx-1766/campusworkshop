45525d65fe8cbc208e743fc35c08954e27d1d851
rdx-1766 <pavankumarmattad@gmail.com>
2023-04-13 14:34:12 +0530

    .git name was changed

--- app/static/script/modal.js
+++ app/static/script/modal.js
@@ -89,11 +89,11 @@ $(document).ready(function () {
         let tID = state.data('source')
         let new_state;
 
-        if (state.text() === "Todo") {
+        if (state.text() == "Todo") {
             new_state = "In Progress"
         }
 
-        if (state.text() === "In Progress") {
+        if (state.text() == "In Progress") {
             new_state = "Completed"
         }
 
